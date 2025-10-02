fbb748bf8152473d30da805b73b94b1241ad07ff
JUAN PABLO LÓPEZ MUÑOZ <JuanPablo.Lopez1@alu.uclm.es>
2025-10-02 13:33:27 +0200

    -Pequeñas modificaciones en la propiedad CustomerUserName. -Se añadieron comentarios en la propiedad CustomerUserName.

--- src/AppForSEII2526.API/Models/Purchase.cs
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -7,8 +7,8 @@ public class Purchase
     [Key]
     public int Id { get; set; } // Primary key, necesaria
 
-    [Display(Name = "Usuario del cliente")]
-    [Required(AllowEmptyStrings = false, ErrorMessage = "Atencion: es necesario que adjunte algun usuario")]
+    [Display(Name = "Nombre del cliente")] //No se como de grande es el nombre del cliente, pero me aseguro de que obligatoriamente utilize esta propiedad
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Atencion: es necesario que adjunte su nombre")] 
     public string CustomerUserName { get; set; } 
 
 }//De clase purchase
