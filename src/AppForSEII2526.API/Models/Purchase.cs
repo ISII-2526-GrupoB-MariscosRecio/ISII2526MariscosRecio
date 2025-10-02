a72ac9582f2e24899f6a73cbe21f3830beb98c48
JUAN PABLO LÓPEZ MUÑOZ <JuanPablo.Lopez1@alu.uclm.es>
2025-10-02 13:07:12 +0200

    Se han añadidos en la propiedad Id de la clase Purchase.cs

--- src/AppForSEII2526.API/Models/Purchase.cs
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -5,6 +5,6 @@ namespace AppForSEII2526.API.Models;
 public class Purchase
 {
     [Key]
-    public int ID { get; set; }
+    public int Id { get; set; } // Primary key, necesaria
 
 }//De clase purchase
