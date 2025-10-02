336b8166ddbad461188a8fa754a15d5c71a7491e
JUAN PABLO LÓPEZ MUÑOZ <JuanPablo.Lopez1@alu.uclm.es>
2025-10-02 13:29:50 +0200

    Se añadió la propiedad CustomerUserName, con sus campos obligatorios

--- src/AppForSEII2526.API/Models/Purchase.cs
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -7,4 +7,8 @@ public class Purchase
     [Key]
     public int Id { get; set; } // Primary key, necesaria
 
+    [Display(Name = "Usuario del cliente")]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Atencion: es necesario que adjunte algun usuario")]
+    public string CustomerUserName { get; set; } 
+
 }//De clase purchase
