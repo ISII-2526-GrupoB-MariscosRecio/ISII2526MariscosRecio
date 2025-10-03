2a8810591932fa92526cb590d51db5cc9ea32ed5
ALEXANDRU COLACEL <Alexandru.Colacel@alu.uclm.es>
2025-10-04 00:48:52 +0200

    He terminado de ajutar todas las restricciones de cada atributo para que no haya errores luego en la base de datos y ademas de comentar las partes del codigo que me faltaban

--- src/AppForSEII2526.API/Models/Rental.cs
+++ src/AppForSEII2526.API/Models/Rental.cs
@@ -12,22 +12,29 @@ namespace AppForSEII2526.API.Models
         [Required(AllowEmptyStrings = false, ErrorMessage = "Porfa tienes que introducir tu direccion")]
         public string DeliveryAddress { get; set; }//hecho
 
-        
+        [Display(Name = "Nombre")]
+        [Required(AlowEmptyStrings = false, ErrorMessage = "Porfa introduce tu nombre")]
         public string Name { get; set; }//asumo que el nombre es imprescindible pero no le voy a poner una longitud definida
 
+        [Display(Name = "Metodos de pago")]
         public PaymentMethodTypes PaymentMethod { get; set; } //hare una enumeracion con los metodos de pago disponibles
 
-       
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha del alquiler")]
         public DateTime RentalDate { get; set; }
 
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de comienzo de alquiler")]
         public DateTime RentalDateFrom { get; set; }
 
-       
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de fin del alquiler")]
         public DateTime RentalDateTo { get; set; }
 
         public string Surname { get; set; } //No vamos a exigir que el apellido sea obligatorio
 
-       
+        [DataType(DataType.Currency)]
+        [Display(Name = "Precio del alquiler")]
         public double TotalPrice { get; set; }
 
     }
