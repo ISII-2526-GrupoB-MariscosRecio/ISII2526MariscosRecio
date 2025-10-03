67841935f9c82928c00affb46c15f6f0d05db1fc
FERNANDO GARCÍA NAVARRO <Fernando.Garcia24@alu.uclm.es>
2025-10-03 12:19:30 +0200

    Clase terminada con todas las propiedades completas

--- src/AppForSEII2526.API/Models/Review.cs
+++ src/AppForSEII2526.API/Models/Review.cs
@@ -2,22 +2,24 @@
 
 public class Review
 {
-    public int CustomerCountryId { get; set; }
+    
+    [Required(AllowEmptyStrings = false, ErrorMessage = "El pais es un campo obligatorio")]
+    public int CustomerCountry { get; set; }//Pais del cliente
 
 
-    public int CustomerId { get; set; }
+    public int CustomerId { get; set; }//Identificador del cliente
 
+    public DateTime DateOfReview { get; set; }//Fecha de la reseña
 
-    public date DateOfReview { get; set; }
+    [Range(1,5, ErrorMessage = "La calificacion tiene que estar entre 1 y 5")]
+    [Required]
+    public int OverallRating { get; set; }//Calificacion que le da el cliente al producto
 
+    [Key]
+    public int ReviewId { get; set; }//Identificador de la reseña único
 
-    public int OverallRating { get; set; }
-
-
-    public int ReviewId { get; set; }
-
-
-    public string ReviewTitle { get; set; }
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Titulo obligatorio para la reseña")]
+    public string ReviewTitle { get; set; }//Titulo de la reseña
 
 
 }
