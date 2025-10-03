9eaf85111375bfaf812eadf2f919563ca279f2cd
FERNANDO GARCÍA NAVARRO <Fernando.Garcia24@alu.uclm.es>
2025-10-03 09:40:15 +0200

    Se han añadido las propiedades simplemente.

---
+++ src/AppForSEII2526.API/Models/Review.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Review
+{
+
+    public int ReviewId { get; set; }
+    public string ReviewTitle { get; set;
+    public int CustomerId { get; set; }
+    public int CustomerCountryId { get; set; }
+    public int OverallRating { get; set; }
+    public date DateOfReview { get; set; }
+
+
+
+}
