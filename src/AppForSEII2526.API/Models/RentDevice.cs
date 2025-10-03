761146fa2fa7c7199817017e90f345628b2f02b7
ALEXANDRU COLACEL <Alexandru.Colacel@alu.uclm.es>
2025-10-04 01:05:30 +0200

    He creado la clase RentDevice que es la ultima que faltaba del caso de uso que se me a asociado aunque faltaria por ver si DeviceId es Key o FK de Rental por lo cual eso lo dejare para ver en el proximo commit

---
+++ src/AppForSEII2526.API/Models/RentDevice.cs
@@ -0,0 +1,16 @@
+Public class RentDevice
+{
+    
+
+       
+    public int DeviceId { get; set; }
+
+    [Required(AllowEmptyStrings = false, ErrorMessage = "El alquiler a de tener un precio")]
+    public double Price { get; set; }
+
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Porfa tienes que introducir una cantidad")]
+    public int Quantity { get; set; }
+
+    public int RentId { get; set; }
+
+}
