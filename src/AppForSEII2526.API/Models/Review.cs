0a5b5748145384911d4fb657d6c9bb0190f24741
FERNANDO GARCÍA NAVARRO <Fernando.Garcia24@alu.uclm.es>
2025-10-05 10:31:08 +0200

    Clase terminada a falta de ver si hay alguna FK.

--- src/AppForSEII2526.API/Models/Review.cs
+++ src/AppForSEII2526.API/Models/Review.cs
@@ -4,23 +4,24 @@
 
 public class Review
 {
+    [Required(AllowEmptyStrings = false, ErrorMessage = "El pasi es un campo obligatorio")]
+    public int CustomerCountry { get; set; }//Pais del cliente que hace la review, lo pongo como int para hacer una enumeracion
 
-    public int CustomerCountryId { get; set; }
- 
-    
-    public int CustomerId { get; set; }
+
+    public int CustomerId { get; set; }//Identificador del cliente
     
     
-    public date DateOfReview { get; set; }
-    
-    
-    public int OverallRating { get; set; }
-    
-    
-    public int ReviewId { get; set; }
-    
-    
-    public string ReviewTitle { get; set; }
+    public date DateOfReview { get; set; }//Fecha de la reseña
+
+    [Range(1,5, ErrorMessage = "La calificación tiene que estar entre 1 y 5")]
+    [Required]
+    public int OverallRating { get; set; }//Calificacion general del cliente, de 1 a 5
+
+    [Key]
+    public int ReviewId { get; set; }//Identificador de la reseña único
+
+    [Required(AllowEmptyStrings = false, ErrorMessage = "El titulo de la reseña es obligatorio")]
+    public string ReviewTitle { get; set; }//Titulo de la reseña obligatorio
 
 
 }
