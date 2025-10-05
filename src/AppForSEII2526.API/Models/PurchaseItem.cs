03f783467285c93f38c1d4dbec2a61e134a8bff6
JUAN PABLO LÓPEZ MUÑOZ <JuanPablo.Lopez1@alu.uclm.es>
2025-10-05 13:31:29 +0200

    -Añadido atributo Description. -Añadido comentarios

--- src/AppForSEII2526.API/Models/PurchaseItem.cs
+++ src/AppForSEII2526.API/Models/PurchaseItem.cs
@@ -7,5 +7,9 @@ public class PurchaseItem
     [Key]
     public int PurchaseId { get; set; } // Primary key, necesaria (TODO: identificar claves foraneas)
 
+    //Las descripciones no deben de ser tan largas, mínimo 50 caracteres, máximo 500 caracteres
+    [Display(Name = "Descripcion")]
+    [Required(500, ErrorMessage = "Atencion: la descripcion no puede ser más larga de 500 caracteres", MinimumLength = 50)]
+    public string Description { get; set; }
 
 }//De clase PurchaseItem
