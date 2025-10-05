213533998f0c99f56bff046454c57e2af87e5c4e
FERNANDO GARCÍA NAVARRO <Fernando.Garcia24@alu.uclm.es>
2025-10-05 11:00:32 +0200

    Definición inicial de las propiedades de la clase ReviewItem.

---
+++ src/AppForSEII2526.API/Models/ReviewItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AppForSEII2526.API.Models
+
+public class ReviewItem
+{
+
+    public string Comments { get; set; }
+
+    public int DeviceId { get; set; }
+
+    public int Id { get; set; }
+
+    public int Rating { get; set; }
+
+    public int ReviewId { get; set; }
+
+}
