3214d718936885a0a60d3600dd3c75f4a2c49920
FERNANDO GARCÍA NAVARRO <Fernando.Garcia24@alu.uclm.es>
2025-10-05 10:06:58 +0200

    Creación de la clase model con sus propiedades.

---
+++ src/AppForSEII2526.API/Models/Model.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace AppForSEII2526.API.Models;
+
+public class Model
+{
+    [Key]
+    public int Id { get; set; }//Identificador único del modelo
+
+
+    [StringLength(30,ErrorMessage="El nombre del modelo no puede tener más de 30 caracteres")]
+    public string NameModel { get; set; }//Nombre del modelo
+
+}
