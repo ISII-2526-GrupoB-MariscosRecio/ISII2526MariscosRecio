70a44fab07f201d1d55e765cc2a1b676faa55349
JUAN PABLO LÓPEZ MUÑOZ <JuanPablo.Lopez1@alu.uclm.es>
2025-10-05 13:20:35 +0200

    -Se añadió el atributo PurchaseID, y se definió como clase primaria. -Se añadieron recordatorios de añadir clasves foráneas si existen.

--- src/AppForSEII2526.API/Models/PurchaseItem.cs
+++ src/AppForSEII2526.API/Models/PurchaseItem.cs
@@ -4,5 +4,8 @@ namespace AppForSEII2526.API.Models;
 
 public class PurchaseItem
 {
-    
+    [Key]
+    public int PurchaseId { get; set; } // Primary key, necesaria (TODO: identificar claves foraneas)
+
+
 }//De clase PurchaseItem
