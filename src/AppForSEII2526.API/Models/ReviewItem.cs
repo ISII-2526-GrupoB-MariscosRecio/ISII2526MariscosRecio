231104c480d6f40f971792fefbf562c80aa87ab6
FERNANDO GARCÍA NAVARRO <Fernando.Garcia24@alu.uclm.es>
2025-10-05 17:04:17 +0200

    Clase ReviewItem con todas sus propiedades y las FK.

--- src/AppForSEII2526.API/Models/ReviewItem.cs
+++ src/AppForSEII2526.API/Models/ReviewItem.cs
@@ -4,15 +4,21 @@ namespace AppForSEII2526.API.Models
 
 public class ReviewItem
 {
+    [Required(AllowEmptyString = false, ErrorMessage = "El comentario no puede estar vacío")]
+    public string Comments { get; set; }//Comentario del review item
 
-    public string Comments { get; set; }
+    [ForeingKey(nameof(DeviceId))]
+    public Device Device { get; set; }//Identificador del dispositivo al que se le hace el review
 
-    public int DeviceId { get; set; }
+    [Key]
+    public int Id { get; set; }//Identificador único del review item
 
-    public int Id { get; set; }
 
-    public int Rating { get; set; }
+    [Range(1,5, ErrorMessage = "La calificación tiene que estar entre 1 y 5")]
+    [Required]
+    public int Rating { get; set; }//Calificación del dispositivo
 
-    public int ReviewId { get; set; }
+    [ForeingKey(nameof(ReviewId))]
+    public Review Review { get; set; }//Identificador de la review a la que pertenece el review item
 
 }
