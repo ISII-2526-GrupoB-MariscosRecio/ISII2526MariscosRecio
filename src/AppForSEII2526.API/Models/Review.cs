a76f1076c90433aed881ac1296e9aae23eb5ed93
FERNANDO GARCÍA NAVARRO <Fernando.Garcia24@alu.uclm.es>
2025-10-05 10:25:14 +0200

    He reordenado las propiedades para que quede un poco más claro.

--- src/AppForSEII2526.API/Models/Review.cs
+++ src/AppForSEII2526.API/Models/Review.cs
@@ -4,11 +4,23 @@
 
 public class Review
 {
-	public int ReviewId { get; set; }
-	public string ReviewTitle { get; set; }
-	public int CustomerId { get; set; }
-	public int CustomerCountryId { get; set; }
-	public int OverallRating { get; set; }
-	public date DateOfReview { get; set; }
+
+    public int CustomerCountryId { get; set; }
+ 
+    
+    public int CustomerId { get; set; }
+    
+    
+    public date DateOfReview { get; set; }
+    
+    
+    public int OverallRating { get; set; }
+    
+    
+    public int ReviewId { get; set; }
+    
+    
+    public string ReviewTitle { get; set; }
+
 
 }
