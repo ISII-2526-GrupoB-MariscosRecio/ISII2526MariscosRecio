d60f7f6368fe46058d5c92272409945c87ee0e82
JUAN PABLO LÓPEZ MUÑOZ <JuanPablo.Lopez1@alu.uclm.es>
2025-10-05 12:21:16 +0200

    -Creación del atributo CustomerUserSurname, con su correspondiente campo obligatorio. -Se han añadido comentarios.

--- src/AppForSEII2526.API/Models/Purchase.cs
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -10,4 +10,8 @@ public class Purchase
     [Display(Name = "Nombre del cliente")] //No se como de grande es el nombre del cliente, pero me aseguro de que obligatoriamente utilize esta propiedad
     [Required(AllowEmptyStrings = false, ErrorMessage = "Atencion: es necesario que adjunte su nombre")]
     public string CustomerUserName { get; set; }
+
+    [Display(Name = "Apellidos del cliente")] //Mismo caso para el nombre del cliente
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Atencion: es necesario que adjunte sus apellidos")]
+    public string CustomerUserSurname { get; set; }
 }
