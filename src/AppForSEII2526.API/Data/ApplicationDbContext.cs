52afa32615480b963ca18f8eed15c3acbd90b378
JUAN PABLO LÓPEZ MUÑOZ <JuanPablo.Lopez1@alu.uclm.es>
2025-10-08 09:07:37 +0200

    -Se ha eliminado la línea builder.Entity<PurchaseItem>().HasKey(pi => new {pi.PurchaseId , pi.DevideId }); dentro de DbContext ya que se ha definido [PrimaryKey(nameof(DeviceId), nameof(PurchaseId))] dentro dePurchaseItem.
    -Se ha especificado explicitamente las clases que quiero asignar en la base de datos ( DbSet<Purchase> Purchase ).

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -6,10 +6,10 @@ namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
     public DbSet PurchaseItem > PurchaseItems { get ; set ; }
+    public DbSet<Purchase> Purchase { get; set; }               //Explicitamente, tengo que asignar las clases que se tienen que asignar en la base de datos.
 
     protected override void OnModelCreating (ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        builder.Entity<PurchaseItem>().HasKey(pi => new {pi.PurchaseId , pi.DevideId }); //Confirmación cambios
     }
 }
