2d3c73c9fb2b77fde9d2ccca4f86fbf154670d71
ALEXANDRU COLACEL <Alexandru.Colacel@alu.uclm.es>
2025-10-08 21:11:35 +0200

    He creado la relacion 1-N de Rental con AplicattionUsers

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -3,19 +3,22 @@
 namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
-//he añadido nombre y apellido a la clase user
+//he añadido nombre y apellido a la clase user 
+//Lo siguiente que voy a hacer 
 public class ApplicationUser : IdentityUser {
-    [Display(Name = "Name")]
+    [Display(Name = "Nombre")]
     public string Name
     {
         get;
         set;
     }
 
-    [Display(Name = "Surname")]
+    [Display(Name = "Apellido")]
     public string Surname
     {
         get;
         set;
     }
+    //FK con Rental
+    public IList<Rental> Rentals { get; set; }
 }
