8c8c8b0ef6a947dce07359fbb2ab6c1bc4bd8793
ALEXANDRU COLACEL <Alexandru.Colacel@alu.uclm.es>
2025-10-08 15:09:32 +0200

    He rellenado la clase ApplicationUsers con nombre y apellidos que eran los datos que faltaban posteriormente haremos cada uno sus respectivas relaciones con esta clase

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -3,5 +3,19 @@
 namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
+//he añadido nombre y apellido a la clase user
 public class ApplicationUser : IdentityUser {
+    [Display(Name = "Name")]
+    public string Name
+    {
+        get;
+        set;
+    }
+
+    [Display(Name = "Surname")]
+    public string Surname
+    {
+        get;
+        set;
+    }
 }
