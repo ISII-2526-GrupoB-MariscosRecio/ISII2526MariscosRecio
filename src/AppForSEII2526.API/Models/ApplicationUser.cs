fc6d344a2d29669714ed96df6c4803a5e871c36c
FERNANDO GARCÍA NAVARRO <Fernando.Garcia24@alu.uclm.es>
2025-10-10 14:33:41 +0200

    Corrección del name space para application user

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -1,29 +1,32 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
-namespace AppForSEII2526.API.Models;
+namespace AppForSEII2526.API.Models
+{
 
-// Add profile data for application users by adding properties to the ApplicationUser class
-//he añadido nombre y apellido a la clase user 
-//Lo siguiente que voy a hacer 
-public class ApplicationUser : IdentityUser {
-    [Display(Name = "Nombre")]
-    public string Name
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    //he añadido nombre y apellido a la clase user 
+    //Lo siguiente que voy a hacer 
+    public class ApplicationUser : IdentityUser
     {
-        get;
-        set;
+        [Display(Name = "Nombre")]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "Apellido")]
+        public string Surname
+        {
+            get;
+            set;
+        }
+        //FK con Rental
+        public IList<Rental> Rentals { get; set; }
+
+        //FK con Purchase (1:N)
+        public IList<Purchase> Purchases { get; set; }
+
+        public IList<Review> Reviews { get; set; }
     }
-
-    [Display(Name = "Apellido")]
-    public string Surname
-    {
-        get;
-        set;
-    }
-    //FK con Rental
-    public IList<Rental> Rentals { get; set; }
-
-    //FK con Purchase (1:N)
-    public IList<Purchase> Purchases { get; set; }
-
-    public IList<Review> Reviews { get; set; }
 }
