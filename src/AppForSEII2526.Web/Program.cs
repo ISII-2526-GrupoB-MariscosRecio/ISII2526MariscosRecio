8d4be03736fcfa98176d0ab49e2926b1077d3f3b
JUAN PABLO LÓPEZ MUÑOZ <JuanPablo.Lopez1@alu.uclm.es>
2025-12-16 21:10:27 +0100

    MODIFICACIONES EN LA IU DE SELECCION DE DISPOSITIVOS CU_Comprar Dispositivo.
    -Correciones en DeviceControler.cs: actualización del método GetDevicesParaPurchase para mapear correctamente name y colo en select de LINQ.
    
    -Alteraciones en Program.cs: uso de Factory Delegate (AddTypedClient) para inyección de URL y resolver una excepción (Unable to resolve service for type 'System.String')
    
    -Actualización de PurchaseStateContainer: migración del contenedor para que use DTOs generados por Swagger.
    
    -Arreglo dentro de la página /compra/selección: Se corrigió el título de la tarjeta para mostrar Marca + Nombre (ej. "Samsung Galaxy S10") en lugar de Marca + Modelo ("Samsung Samsung"); y se añadió el atributo [AllowAnonymous] para permitir el acceso y pruebas sin necesidad de login previo (temporal/desarrollo).

--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -1,4 +1,5 @@
 using AppForSEII2526.Web;
+using AppForSEII2526.Web.API; // Para registrar el cliente HTTP
 using AppForSEII2526.Web.Components;
 using AppForSEII2526.Web.Components.Account;
 using AppForSEII2526.Web.Data;
@@ -35,6 +36,21 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.Requ
     .AddDefaultTokenProviders();
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+
+// IMPORTANTE: El puerto 7067 sale de tu launchSettings.json de la API (https)
+
+//MODIFICACIONES: debido a que el mero de presionar la pestaña donde tenemos la selección de compras provocaba la combustión instantánea de mi pantalla, he decidido usar el pratón Factory:
+
+//Añadimos un cliente estandar (AddHttpClient), del tipo generado por Swagger. Tras mirar el constructor (http, sp) dentro de la definición,
+// me doy cuenta que el sistema no sabe que poner dentro de sp, por lo que tenemos que darle la url manualmente.
+
+builder.Services.AddHttpClient<AppForSEII2526APIClient>()
+    .AddTypedClient<AppForSEII2526APIClient>((http, sp) =>
+    {
+        var apiUrl = "https://localhost:7067/";
+        return new AppForSEII2526APIClient(apiUrl, http);
+    });
 
 // Registramos el StateContainer para gestionar el estado de la compra (carrito)
 builder.Services.AddScoped<PurchaseStateContainer>();
